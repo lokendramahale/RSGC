af9bc2fcb2f811cacff2474f8719393721c3f9a6
Lokendra Mahale <lokendramahale@gmail.com>
2025-07-22 22:23:06 +0530

    deployment issue resolved

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  // eslint: {
+  //   ignoreDuringBuilds: true, // Optional: disable lint error on Vercel
+  // },
+};
+
+module.exports = nextConfig;
