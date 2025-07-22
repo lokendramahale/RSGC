1d397bc1322474c3eb260ab9f02901118ee23dec
Lokendra Mahale <lokendramahale@gmail.com>
2025-07-22 23:37:30 +0530

    build error resolved

--- config/database.js
+++ config/database.js
@@ -2,21 +2,21 @@ const { Pool } = require("pg")
 require("dotenv").config()
 
 //for supabase 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false, // Supabase requires SSL
-//   },
-// });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Supabase requires SSL
+  },
+});
 
 // For local database
-const pool = new Pool({
-  user: process.env.DB_USER,        
-  host: process.env.DB_HOST,        
-  database: process.env.DB_NAME,   
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,        
-});
+// const pool = new Pool({
+//   user: process.env.DB_USER,        
+//   host: process.env.DB_HOST,        
+//   database: process.env.DB_NAME,   
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,        
+// });
 
 // Test connection
 pool.on("connect", () => {
